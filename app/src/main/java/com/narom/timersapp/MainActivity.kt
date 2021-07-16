fd7b79b3c2508b9f390cadd7386362994128947d
narom kuoch <naromkuoch@naroms-MacBook-Air.local>
2021-07-15 22:13:16 -0500

    Initial commit

---
+++ app/src/main/java/com/narom/timersapp/MainActivity.kt
@@ -0,0 +1,38 @@
+package com.narom.timersapp
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import com.narom.timersapp.ui.theme.TimersAppTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            TimersAppTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(color = MaterialTheme.colors.background) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello $name!")
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    TimersAppTheme {
+        Greeting("Android")
+    }
+}
